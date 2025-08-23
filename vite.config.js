2878511caf1a387db4295494d00c7b2778a12295
ahmad alkheder <ahmadalkhder2002g.2@gmail.com>
2025-08-23 15:19:10 +0300

    feat: Add Vite server configuration3

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     server: {
         hmr: {
-            host: 'b9ba92a5-b947-42b7-8f51-da6c7a7eed71-00-3ccxgzfs6cyrv.sisko.replit.dev',
+            host: process.env.REPL_SLUG
+                ? `${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.replit.dev`
+                : 'localhost',
             clientPort: 443,
         },
         host: '0.0.0.0',
-        allowedHosts: ['.replit.dev']
+        allowedHosts: ['all'], // السماح لأي دومين (ديناميكي)
     },
     plugins: [
         laravel({
